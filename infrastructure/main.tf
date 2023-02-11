5a1a3a402a0327b224f43ef27137ff10ce599848
Jim Cumming <jim@jimc.co.uk>
2023-02-11 18:16:08 +0000

    Apply more secure ssl policy

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -239,6 +239,7 @@ resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.loadbalancer.arn
   port              = 443
   protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-FS-1-2-Res-2020-10"
   certificate_arn   = aws_acm_certificate_validation.cert.certificate_arn
 
   default_action {
